13ab1e15596571c90b1e39c035d023debea22278
Lucas Neves <lucas54neves@gmail.com>
2022-10-17 22:37:03 -0300

    test: update address test

--- src/test/models/test_address.py
+++ src/test/models/test_address.py
@@ -6,3 +6,8 @@ def test_new_address():
     
     assert _address.address == 'ABC123'
     assert _address.crypto == 'BTC'
+
+    address_json = _address.json()
+
+    assert address_json['address'] == 'ABC123'
+    assert address_json['crypto'] == 'BTC'
