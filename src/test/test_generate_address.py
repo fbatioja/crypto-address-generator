2ea7bf73c24f3e13631227381c3d04f5c5bfc13d
Lucas Neves <lucas54neves@gmail.com>
2022-10-17 22:25:40 -0300

    fix: update tests

--- src/test/test_generate_address.py
+++ src/test/test_generate_address.py
@@ -6,7 +6,7 @@ from utils.generate_address import generate_address
 
 class TestingGenerateAddress(TestCase):
     def test_address_generation(self):
-        address = generate_address('BTC')
+        address = generate_address('BTC', '33353816f44447ede1a74fc278b9b4bda415218b86ffe8166b0cba65c1ee5510')
         
         self.assertIsNotNone(address)
         self.assertIsInstance(address, str)
